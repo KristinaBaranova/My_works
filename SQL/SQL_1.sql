11d5d83913fca3da746c740551f4cf41a946dbde
KristinaBaranova <kristina.fwrk@gmail.com>
2022-12-14 16:12:59 +0700

    update SQL_1.sql file

--- SQL/SQL_1.sql
+++ SQL/SQL_1.sql
@@ -40,18 +40,24 @@ INSERT ALL
  INTO ORDERS (ONUM, AMT, ODATE, CNUM, SNUM) VALUES (3011, 9891.88, to_date('06.01.2020','dd.mm.yyyy'), 2006, 1001)
 SELECT* FROM dual;
 
-1.
+Запросы
+
+1.	Выведите всех покупателей с рейтингом выше 100, проживающих в городах, в названии
+которых вторая буква не равна o ””, а четвертая буква не равна e
+
 SELECT* FROM CUSTOMER
 WHERE RATING>100 AND CITY NOT LIKE '_o_e'
 
-2.
+2.	Запросите двумя способами все заказы на 3 и 4 января
+
 SELECT* FROM ORDERS
 WHERE ODATE='03-JAN-20' OR ODATE='04-JAN-20'
 
 SELECT* FROM ORDERS
 WHERE ODATE IN ('03-JAN-20','04-JAN-20')
 
-3.
+3.	Выведите сумму самого раннего заказа за каждую дату
+
 SELECT AMT, ONUM, ODATE
 FROM ORDERS
 WHERE ONUM IN
@@ -59,29 +65,39 @@ WHERE ONUM IN
 FROM ORDERS
 GROUP BY ODATE)
 
-4.
+4.	Напишите запрос, который сосчитал бы сумму всех заказов для покупателей, которых
+обслуживает продавец с именем Peel
+
 SELECT SUM(AMT) FROM ORDERS
 WHERE SNUM IN
 (SELECT SNUM FROM SELLER WHERE SNAME='Peel')
 
-5.
+5.	Напишите запрос, который выводит все заказы, сумма которых больше средней по всем
+заказам, используя подзапрос
+
 SELECT * FROM ORDERS
 WHERE AMT >
 (SELECT AVG(AMT) FROM ORDERS)
 
-6.
+6.	Напишите запрос, который вывел бы для каждого заказа его номер, стоимость и имя
+заказчика. Данные вывести для заказчиков, размещенных не в Лондоне и не в Риме
+
 SELECT ORDERS.ONUM, ORDERS.CNUM, ORDERS.AMT, CUSTOMER.CNAME
 FROM ORDERS, CUSTOMER
 WHERE ORDERS.CNUM=CUSTOMER.CNUM
 AND NOT CUSTOMER.CITY='London' AND NOT CUSTOMER.CITY='Rome'
 
-6.2
+или
+
 SELECT ORDERS.ONUM, ORDERS.CNUM, ORDERS.AMT, CUSTOMER.CNAME FROM ORDERS
 INNER JOIN CUSTOMER ON ORDERS.CNUM=CUSTOMER.CNUM
 WHERE NOT CUSTOMER.CITY='London' AND NOT CUSTOMER.CITY='Rome'
 
 
-7.
+7.	Вывести пары имен покупатель продавец, которые совершили сделки не 4 и не 6
+января, при этом сумма каждой сделки от 1000 до 5000. Отсортировать по возрастанию
+суммы. Без использования подзапросов
+
 SELECT CUSTOMER.CNAME, SELLER.SNAME
 FROM CUSTOMER, SELLER, ORDERS
 WHERE ORDERS.CNUM=CUSTOMER.CNUM AND ORDERS.SNUM=SELLER.SNUM
@@ -89,7 +105,8 @@ AND NOT ORDERS.ODATE='04-JAN-20' AND NOT ORDERS.ODATE='06-JAN-20'
 AND ORDERS.AMT BETWEEN 1000 AND 5000
 ORDER BY ORDERS.AMT
 
-7.2
+или
+
 SELECT CUSTOMER.CNAME, SELLER.SNAME FROM ORDERS
 INNER JOIN SELLER ON ORDERS.SNUM=SELLER.SNUM
 INNER JOIN CUSTOMER ON ORDERS.CNUM=CUSTOMER.CNUM
@@ -98,7 +115,9 @@ AND ORDERS.AMT BETWEEN 1000 AND 5000
 ORDER BY ORDERS.AMT
 
 
-8.
+8.	Вывести сумму сделки и имена покупателей, которые совершили сделку 3 января, но в
+городе отличном от города продавца.
+
 SELECT ORDERS.AMT, CUSTOMER.CNAME
 FROM ORDERS, CUSTOMER, SELLER
 WHERE ORDERS.CNUM=CUSTOMER.CNUM AND ORDERS.SNUM=SELLER.SNUM
